4e2e5a0338735e9ef2da609a63ee0c7a16699032
samiyamarium <160397803+samiyamarium@users.noreply.github.com>
2024-11-14 12:04:42 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <div className=' rounded-md bg-gray-100 text-pink-700  outline-dashed outline-4 font-extrabold text-2xl font-serif text-center pl-4 '>CLASS 9 ASSIGNMENT <br/><br/> 2 cards in first row <br/><br/> 3 cards in second row</div>
+     
+   <br/><div className='flex'><div className="mx-5  flex-1 w-1/2 h-96 rounded-md  text-white bg-[url('/new7.png')] bg-cover"></div><div className="  mx-5 flex-1 w-1/2 h-96 rounded-md bg-[url('/new8.png')] bg-cover"></div></div>
+   <br/><div className='flex'><div className="mx-5 p-10 flex-1 w-60 h-96 rounded-md  text-white bg-[url('/new4.png')] bg-cover"></div><div className="   mx-5 flex-1 w-60 h-96 rounded-md bg-[url('/new5.png')] bg-cover"></div><div className= "mx-5 rounded-md flex-1 w-60 h-96 bg-[url('/new6.png')] bg-cover"></div></div>
+   
+    </div>
+    
+  );
+}
